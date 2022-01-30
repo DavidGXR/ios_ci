37c8a2943a624d062b29553d8cfd9a294259d20e
David Im <im.david@yahoo.com>
2022-01-30 12:52:17 +0700

    Initial commit

---
+++ ios_ci/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_ci
+//
+//  Created by David Im on 30/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
